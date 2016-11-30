e27825aeffd0f9fc72a9e925053ae3bdf2b7bc0e
siafei <376046266@qq.com>
2016-11-30 13:58:25 +0800

    create sqlClass

---
+++ phpSpider/SqlClass.php
@@ -0,0 +1,50 @@
+<?php
+
+class sql{
+
+	private static $instance ;
+
+
+
+	public function __construct(){
+		if(empty(self::$instance)){
+			self::$instance = new self ;
+		}
+	}
+
+	public function set(){	
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
