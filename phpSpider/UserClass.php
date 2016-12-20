8010aacf88f0ccf07711a5e73fb6d2637fba3459
siafei <376046266@qq.com>
2016-12-20 11:56:16 +0800

    add method

--- phpSpider/UserClass.php
+++ phpSpider/UserClass.php
@@ -2,45 +2,62 @@
 class UserClass{
     private static $instance ;
     private static $cookie ;
-    private static $useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' ;
-    public function getinstances($cookie){
+	private static $header ;
+	public static function getinstances($cookie){
         if(empty($instance)){
             self::$cookie = $cookie ;
+			self::setHeader(self::$cookie) ;
             self::$instance = new self ; 
         }
         return self::$instance ;
     }
-    // 获取用户信息
-    public function getUserInfo($uname){
-        $ch = curl_init('https://www.zhihu.com/people/'.$uname) ;
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_COOKIE, self::$cookie) ;
-        curl_setopt($ch, CURLOPT_USERAGENT, self::$useragent);
-        $info = curl_exec($ch) ;
-        return $info ;
-    }
-    //获取用户关注着
-    public function getFollowes(){
-         $header[] = 'method:next' ;
-         $header[] = 'params:{"offset":40,"order_by":"created","hash_id":"64e29e1315485906c1a2b610f7ab741d"}' ;
-         $ch = curl_init('www.zhihu.com/node/ProfileFolloweesListV2') ;
-         curl_setopt($ch, CURLOPT_AUTOREFERER, 'www.zhihu.com/people/si-a-fei/followees');
-         curl_setopt($ch, CURLOPT_HTTPGET, 'next');
-         curl_setopt($ch, CURLOPT_COOKIE, self::$cookie);
-         curl_setopt($ch, CURLOPT_USERAGENT, self::$useragent);
-         $info = curl_exec($ch) ;
-         return $info ;
-    }
 
-    //获取用户关注了哪些人
-    public function getFollowees($uname){
-        $ch = curl_init('https://www.zhihu.com/people/'.$uname.'/followees') ;
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_COOKIE, self::$cookie);
-        curl_setopt($ch, CURLOPT_USERAGENT, self::$useragent);
-        $info = curl_exec($ch) ;
-        return $info ;
-    }
+	public function sendHttp($url,$method="get",$params=array()){
+		$ch = curl_init() ;
+		curl_setopt($ch,CURLOPT_URL,$url) ;
+		//关掉证书认证
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		//设置已变量形式返回
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch,CURLOPT_HTTPHEADER,self::$header);
+		switch($method){
+			case "get":
+				curl_setopt($ch,CURLOPT_HTTPGET,true);
+				break ;
+			case "post":
+				curl_setopt($ch,CURLOPT_POST,true);
+				curl_setopt($ch,CURLOPT_POSTFIELDS,$params);
+				break ;
+			case "delete":
+				curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+				curl_setopt($ch, CURLOPT_POSTFIELDS,$params);
+				break ;
+			case "put":
+				curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+				curl_setopt($ch, CURLOPT_POSTFIELDS,$params);
+				break ;
+		}
+		$response = curl_exec($ch);
+		return $response ;
+		curl_close($ch);
+	}
+
+	private static function setHeader($cookies){
+		$cookie = "Cookie:".$cookies ;
+		self::$header = array(
+			$cookie,
+			'Host:www.zhihu.com',
+			'Referer:https://www.zhihu.com/',
+			'User-Agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+			'Upgrade-Insecure-Requests:1',
+			'authorization:Bearer Mi4wQUFBQUNxb3JBQUFBZ0FCQUFUUjZDaGNBQUFCaEFsVk5vMDk0V0FEVmtmYVZFOW1MU2d3bG40c1BpaHdCVk1taWJn|1482134526|9bf60095e42eac0eed82c2bf9f436e3cfb273340'
+		) ;	
+	
+	}
+
+
+
 }
 
 ?>
