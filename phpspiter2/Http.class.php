7c56c7325b7aed391f0aac2a35c8ab2e0856a3f9
siafei <376046266@qq.com>
2017-01-01 16:16:59 +0800

    11

---
+++ phpspiter2/Http.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class HttpClass{
+
+private $header ;
+
+
+public function __construct(){
+		
+		
+		
+		}
+		
+public function sendpost($url,$param='',$head=''){}
+		
+public function sendget($url,$header){}
+
+public function sendHttp($url,$type='post',$param='',$head){}
+
+public function setHeader($param){}
+
+
+
+}
+
+
+
+
+
+?>
